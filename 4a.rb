74de8586cf401e4b403bc254b1109e3489804235
smelge <traildogfilms@gmail.com>
2018-10-08 10:53:01 +0100

    Added basic homepage and navigation

---
+++ 4a.rb
@@ -0,0 +1,10 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+require_relative('./controllers/adoption_controller.rb')
+require_relative('./controllers/animal_controller.rb')
+require_relative('./controllers/customer_controller.rb')
+
+get('/') do
+  erb(:index)
+end
